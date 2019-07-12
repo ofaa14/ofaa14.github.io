d15234aa798185a442c7929352c538c0e0780065
Ofa Ahofono <796206@seq.org>
2019-07-12 10:13:42 -0700

    added javascript file

---
+++ my_app.js
@@ -0,0 +1,6 @@
+document.getElementById(id)
+
+var name, age, height;    // Statement 1
+name = "Ofa Ahofono";          // Statement 2
+age = 17;          // Statement 3
+height = 5.9;      // Statement 4
